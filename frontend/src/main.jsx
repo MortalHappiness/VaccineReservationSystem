9bcb285195dd373b0eac6703c03dbe20f7683de2
jeff-901 <b07901052@ntu.edu.tw>
2022-05-31 16:32:27 +0800

    add router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import "flowbite";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>
 );
