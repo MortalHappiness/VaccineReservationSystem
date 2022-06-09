6e84e4e84a6f3ada2cb0006a35f1e46ec73c1d04
Kenchu123 <k88913n@gmail.com>
2022-06-09 11:27:46 +0800

    fix: typo

--- bigtable/main.go
+++ bigtable/main.go
@@ -250,6 +250,6 @@ func main() {
 	// insertUserRandom(vaccineClient, 10)
 	
 	// batchInsertUserFromCsvFile(vaccineClient, "data/user_10000.csv")
-	batchInsertUserFromCsvFile(vaccineClient, "data/user_1000000.csv")
+	batchInsertUserFromCsvFile(vaccineClient, "data/user_300000.csv")
 	batchInsertHospitalFromCsvFile(vaccineClient, "data/hospitals_all_clean.csv")
 }
