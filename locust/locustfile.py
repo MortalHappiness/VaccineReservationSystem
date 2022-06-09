76fc54f285ce7d16f18167030db0162575a3e526
Kenchu123 <k88913n@gmail.com>
2022-06-09 16:15:07 +0800

    fix: modify locust

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,3 +1,4 @@
+
 from locust import HttpUser, between, task
 import csv
 import uuid
@@ -6,14 +7,18 @@ import time
 import json
 users = []
 hospitals = []
-with open("/mnt/locust/data/user_10000.csv", "r", newline='') as csvfile:
+with open("/locust-tasks/data/user_1000000.csv", "r", newline='') as csvfile:
     rows = csv.reader(csvfile)
+    cnt = 0
     for row in rows:
+        if cnt > 100000:
+            break
+        cnt += 1
         if row[0] == "ID":
             continue
         users.append(row)
         # ID,name,healthCardID,gender,birthDay,address,phone,vaccines
-with open("/mnt/locust/data/hospitals_all_clean.csv", "r", newline='') as csvfile:
+with open("/locust-tasks/data/hospitals_all_clean.csv", "r", newline='') as csvfile:
     rows = csv.reader(csvfile)
     for row in rows:
         if row[0] == "ID":
@@ -22,53 +27,56 @@ with open("/mnt/locust/data/hospitals_all_clean.csv", "r", newline='') as csvfil
         #ID,county,township,name,address,vaccineCnt
 
 
-
-
 class WebsiteUser(HttpUser):
-    wait_time = between(0.5, 10)
-    user = None
+    wait_time = between(10, 20)
     
     def on_start(self):
+        self.login = False
         self.user = random.choice(users)
-        self.client.post("/api/session", json={
+        
+        response = self.client.post("/api/session", json={
             "nationID": self.user[0],
             "healthCardID": self.user[2]
         })
+        if response.status_code < 400:
+            self.login = True
     
-    @task
-    def index(self):
-        self.client.get("/api/session")
+    @task(10)
+    def frontend(self):
+        self.client.get("/")
 
-    @task(2)
-    def get_hospita(self):
-        hospital = random.choice(hospitals)
-        self.client.get("/api/hospitals", params={
-            "county": hospital[1],
-            "township": hospital[2]
-        })
-
-    
-    @task(3)
-    def get_hospital_and_post_reservation(self):
-        hospital = random.choice(hospitals)
-        # get hospital
-        self.client.get("/api/hospitals", params={
-            "county": hospital[1],
-            "township": hospital[2]
-        })
-        
-        # post reservation
-        self.client.post(f"/api/reservations/users/{self.user[0]}", json={
-            "id": str(uuid.uuid1()),
-            "user": {
-                "nationID": self.user[0]
-            },
-            "hospital": {
-                "id": hospital[0],
+    @task(10)
+    def get_hospital(self):
+        if self.login:
+            hospital = random.choice(hospitals)
+            self.client.get("/api/hospitals", params={
                 "county": hospital[1],
                 "township": hospital[2]
-            },
-            "vaccineType": random.choice(list(json.loads(hospital[-1]).keys())),
-            "completed": False,
-            "date": int(time.time())
-        })
+            })
+
+    
+    @task(6)
+    def get_hospital_and_post_reservation(self):
+        if self.login:
+            hospital = random.choice(hospitals)
+            # get hospital
+            self.client.get("/api/hospitals", params={
+                "county": hospital[1],
+                "township": hospital[2]
+            })
+            
+            # post reservation
+            self.client.post(f"/api/reservations/users/{self.user[0]}", json={
+                "id": str(uuid.uuid1()),
+                "user": {
+                    "nationID": self.user[0]
+                },
+                "hospital": {
+                    "id": hospital[0],
+                    "county": hospital[1],
+                    "township": hospital[2]
+                },
+                "vaccineType": random.choice(list(json.loads(hospital[-1]).keys())),
+                "completed": False,
+                "date": int(time.time())
+            })
